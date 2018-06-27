4b5d5ef98f84768a455775cac014f13ba746bac0
Marco Caimi <mcaimi@redhat.com>
2018-06-27 10:09:54 +0200

    [+] Fixed install script. Many things were missing

--- install.sh
+++ install.sh
@@ -7,16 +7,16 @@ DASHPATH="$SWPATH/dashboards/project"
 for host in "$@"; do
   # install api integration python file
   ssh -i $SSHKEY root@$host mkdir -p $APIPATH
-  scp -i $SSHKEY root@$host:$APIPATH/
+  scp -i $SSHKEY api/designate.py root@$host:$APIPATH/
 
   # install dashboard
-  scp -ri $SSHKEY root@$host:$DASHPATH/
+  scp -ri $SSHKEY dns root@$host:$DASHPATH/
 
   # enable dashboard
-  scp -i $SSHKEY root@$host:$SWPATH/enabled/
+  scp -i $SSHKEY enabled/_3032_network_dns_panel.py root@$host:$SWPATH/enabled/
 
   # install policy file
-  scp -i $SSHKEY root@$host:/etc/openstack-dashboard/dns_policy.json
+  scp -i $SSHKEY dns_policy.json root@$host:/etc/openstack-dashboard/dns_policy.json
 
   # fix selinux
   ssh -i $SSHKEY root@$host restorecon -Rv $SWPATH
@@ -28,5 +28,5 @@ for host in "$@"; do
 done
 
 # Now upload the new policy file to the server that hosts the designate-api service.
-scp -i $SSHKEY root@$DNSMASTER:/etc/designate/policy.json
+scp -i $SSHKEY dns_policy.json root@$DNSMASTER:/etc/designate/policy.json
 
